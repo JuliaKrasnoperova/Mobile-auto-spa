073dd8928a0e192aa2973edafe3982b13541eaee
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-06-12 23:21:39 +0500

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 // preloader
 window.onload = function () {
   let preloader = document.getElementById("preloader");
@@ -5,34 +6,66 @@ window.onload = function () {
 };
 
 window.addEventListener("DOMContentLoaded", () => {
+  // form Validation
+  const form = document.querySelectorAll(".form");
+  const inputArea = document.querySelectorAll(".input");
+
+  function error() {
+    inputArea.forEach((item) => {
+      item.classList.add("error");
+    });
+  }
+
+  function validation(form) {
+    let result = true;
+
+    const allInputs = form.querySelectorAll("input");
+    for (const input of allInputs) {
+      if (input.value == "") {
+        console.log("error");
+        error();
+        result = false;
+      }
+    }
+
+    return result;
+  }
+
+  form.forEach((item) => {
+    item.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      if (validation(this) === true) {
+        inputArea.forEach((item) => {
+          item.classList.remove("error");
+        });
+      }
+    });
+  });
+
   // Tabs
   const tabs = document.querySelectorAll(".tabs__item");
   const tabsContent = document.querySelectorAll(".tabs__content");
   const tabsParent = document.querySelector(".tabs__list");
-
   function hideTabContent() {
     tabsContent.forEach((item) => {
       item.classList.add("hide");
       item.classList.remove("show", "fade");
     });
-
     tabs.forEach((item) => {
       item.classList.remove("tabs__item_active");
     });
   }
-
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
+  function showTabContent(i) {
+    tabsContent[i].classList.add("show");
+    tabsContent[i].classList.add("fade");
     tabsContent[i].classList.remove("hide");
     tabs[i].classList.add("tabs__item_active");
   }
-
   hideTabContent();
-  showTabContent();
-
+  showTabContent(0);
   tabsParent.addEventListener("click", (event) => {
     const target = event.target;
-
     if (target && target.classList.contains("tabs__item")) {
       tabs.forEach((item, i) => {
         if (target == item) {
