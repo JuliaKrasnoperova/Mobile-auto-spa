4cd990694a2ac5fb57e3f66734fd5843a4626ab5
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-06-15 22:16:34 +0500

    form validation has been changed

--- index.js
+++ index.js
@@ -7,89 +7,90 @@ window.onload = function () {
 
 window.addEventListener("DOMContentLoaded", () => {
   // form Validation
-  const form = document.querySelectorAll(".form");
-  const inputArea = document.querySelectorAll(".input");
-  const textArea = document.querySelectorAll(".form__textarea");
+  const modal = document.querySelector(".popup");
+  const overlay = document.querySelector(".popup__bg");
+  const modalClose = document.querySelector(".popup__close");
+  const body = document.querySelector("body");
 
-  function error() {
-    inputArea.forEach((item) => {
-      item.classList.add("error");
-    });
-  }
+  const openModal = () => {
+    overlay.classList.add("active");
+    modal.classList.add("popup__active");
+    body.classList.add("_locked");
+  };
 
-  function removeError() {
-    inputArea.forEach((item) => {
-      item.classList.remove("error");
-    });
-  }
+  // targets all the classes and id from the HTML
+  const id = (id) => document.getElementById(id);
+  const classes = (classes) => document.getElementsByClassName(classes);
 
-  function cleanForm() {
-    inputArea.forEach((input) => {
-      input.value = "";
-    });
-    textArea.forEach((textArea) => {
-      textArea.value = "";
-    });
-  }
+  // stores classes and id's in variables
+  const userName = id("username");
+  const email = id("tel");
+  const form = id("form");
+  const errorMsg = classes("error");
 
-  function validation(form) {
-    let result = true;
-    removeError();
-    const allInputs = form.querySelectorAll(".input");
+  // targets form submit button
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    allInputs.forEach((item) => {
-      if (item.dataset.require == "true") {
-        if (item.value == "") {
-          console.log("error");
-          error();
-          result = false;
-        }
-      }
-    });
+    engine(userName, 0, "Username cannot be blank");
+    engine(tel, 1, "Please enter a valid cell phone number");
 
-    return result;
-  }
-
-  form.forEach((item) => {
-    item.addEventListener("submit", function (event) {
-      event.preventDefault();
-
-      if (validation(this) === true) {
-        cleanForm();
-      }
-    });
+    if (userName.value.trim() !== "" && email.value.trim() !== "") {
+      openModal();
+      modalClose.addEventListener("click", () => {
+        overlay.classList.remove("active");
+        modal.classList.remove("popup__active");
+        body.classList.remove("_locked");
+      });
+      form.reset();
+    }
   });
+
+  const engine = (id, serial, message) => {
+    if (id.value.trim() === "") {
+      errorMsg[serial].innerHTML = message;
+      id.style.border = "2px solid red";
+    } else {
+      errorMsg[serial].innerHTML = "";
+      id.style.border = "2px solid green";
+    }
+  };
 
   // Tabs
   const tabs = document.querySelectorAll(".tabs__item");
   const tabsContent = document.querySelectorAll(".tabs__content");
   const tabsParent = document.querySelector(".tabs__list");
-  function hideTabContent() {
-    tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
-    });
-    tabs.forEach((item) => {
-      item.classList.remove("tabs__item_active");
-    });
-  }
-  function showTabContent(i) {
-    tabsContent[i].classList.add("show");
-    tabsContent[i].classList.add("fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabs__item_active");
-  }
-  hideTabContent();
-  showTabContent(0);
-  tabsParent.addEventListener("click", (event) => {
-    const target = event.target;
-    if (target && target.classList.contains("tabs__item")) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
-        }
+  if (document.contains(tabsParent)) {
+    function hideTabContent() {
+      tabsContent.forEach((item) => {
+        item.classList.add("hide");
+        item.classList.remove("show", "fade");
+      });
+
+      tabs.forEach((item) => {
+        item.classList.remove("tabs__item_active");
       });
     }
-  });
+
+    function showTabContent(i = 0) {
+      tabsContent[i].classList.add("show", "fade");
+      tabsContent[i].classList.remove("hide");
+      tabs[i].classList.add("tabs__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+    tabsParent.addEventListener("click", (event) => {
+      const target = event.target;
+
+      if (target && target.classList.contains("tabs__item")) {
+        tabs.forEach((item, i) => {
+          if (target == item) {
+            hideTabContent();
+            showTabContent(i);
+          }
+        });
+      }
+    });
+  }
 });
