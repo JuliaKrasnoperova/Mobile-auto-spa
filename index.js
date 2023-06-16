df50f054114816d2efe9c9760b4a1f9802a6f05d
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-06-16 21:44:27 +0500

    changes

--- index.js
+++ index.js
@@ -93,4 +93,37 @@ window.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
+
+  // Accordion
+  const accordion_item = document.querySelectorAll(".accordion__item");
+
+  accordion_item.forEach((item) => {
+    const accordion_header_item = item.querySelector(".accordion__header");
+
+    accordion_header_item.addEventListener("click", () => {
+      const accordion_content_item = item.querySelector(".accordion__content");
+
+      const item_actived = document.querySelector(".accordion__active");
+
+      VerifyActive(item, accordion_content_item, item_actived);
+    });
+  });
+
+  function VerifyActive(item, content, content_actived) {
+    const icon_item = item.querySelector(".accordion__icon");
+    const icon_item_active = document.querySelectorAll(".accordion__icon");
+
+    icon_item_active.forEach((item) => (item.innerHTML = "+"));
+
+    if (content_actived) {
+      content_actived.style.height = 0;
+      content_actived.classList.remove("accordion__active");
+    }
+
+    if (content !== content_actived) {
+      icon_item.innerHTML = "-";
+      content.classList.add("accordion__active");
+      content.style.height = content.scrollHeight + 10 + "px";
+    }
+  }
 });
