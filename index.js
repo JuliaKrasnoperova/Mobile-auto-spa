6ae1afed0da7c6dbf63d2b922e43c6b53d7493d6
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-06-24 23:18:27 +0500

    changes

--- index.js
+++ index.js
@@ -166,4 +166,26 @@ window.addEventListener("DOMContentLoaded", () => {
       thisSlide(sliderCount);
     });
   });
+
+  // Video
+
+  const links = document.querySelectorAll("#video");
+
+  links.forEach(function (video) {
+    video.addEventListener("click", () => {
+      if (video.classList.contains("ready")) {
+        return;
+      }
+
+      const src = video.dataset.src;
+
+      video.classList.add("ready");
+      video.insertAdjacentHTML(
+        "afterbegin",
+        `<iframe src= ${src} title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen></iframe>`
+      );
+    });
+  });
 });
