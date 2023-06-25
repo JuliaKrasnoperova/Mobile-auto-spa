c4b4ae7ad6efdd9f35bd5b4ffe01b9adf7e5a93a
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-06-25 13:25:01 +0500

    changes

--- mail.php
+++ mail.php
@@ -3,9 +3,8 @@ $fio = $_POST['FIO'];
 $phone = $_POST['PHONE'];
 $text = $_POST['TEXT'];
 $headers = 'From: webmaster@example.com';
-mail("julia.krasnoperova97@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Телефон: ".$phone.". Сообщение: ".$text,$headers);
+mail("julia.krasnoperova97@gmail.com", "Заявка с сайта Mobile Auto SPA", "ФИО:" . $fio . ". Телефон: " . $phone . ". Сообщение от пользователя: " . $text, $headers);
 
 
-header("Location: index.html");
+header("Location: thankyou.html");
 exit();
-
